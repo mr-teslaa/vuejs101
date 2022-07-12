1a2d8509f8428721ab9f9368e2f9babb7d3b2ba8
mr-teslaa <mr.teslaa@protonmail.com>
2022-07-12 10:41:42 +0600

    finished lesson-2 modal

--- src/App.vue
+++ src/App.vue
@@ -4,40 +4,77 @@
     <hr />
     <input type="text" ref="name" />
     <button @click="handleClick">Change the input</button>
-    <Modal />
+
+    <!-- modal version 1.0 -->
+    <!-- <div v-if="showModal">
+        <Modal
+            :header="header"
+            :text="text"
+            theme="sale"
+            @close="toggleModal"
+        />
+    </div> -->
+
+    <!-- modal version 2.0 -->
+    <div v-if="showModal">
+        <Modal theme="sale" @close="toggleModal">
+            <!-- whatever template passing inside the MODAL tag is called slots -->
+
+            <!-- we can give name of slot -->
+            <template v-slot:links>
+                <a href="#">Take the offer</a>
+
+                <a href="#">Explore more giveaway</a>
+            </template>
+
+            <!-- if we delete this the default slot will work -->
+            <!-- <h1>Special Offer</h1>
+            <p>Grab your ninja swag for free 🎉</p> -->
+        </Modal>
+    </div>
+    <br />
+    <br />
+
+    <button @click.alt="toggleModal">Open Modal (alt+click)</button>
 </template>
 
 <script>
-// importing the Modal.vue file
-import Modal from "./components/Modal.vue";
+    // importing the Modal.vue file
+    import Modal from "./components/Modal.vue";
 
-export default {
-    name: "App",
-    components: { Modal },
-    data() {
-        return {
-            boo: "vue.js",
-        };
-    },
-
-    methods: {
-        handleClick() {
-            // syntex of accessing the refs
-            console.log(this.$refs.name);
-            this.$refs.name.classList.add("active");
-            this.$refs.name.focus();
+    export default {
+        name: "App",
+        components: { Modal },
+        data() {
+            return {
+                boo: "vue.js",
+                header: "Signup for the giveway",
+                text: "Grab your ninja swag fro half price",
+                showModal: false,
+            };
         },
-    },
-};
+
+        methods: {
+            handleClick() {
+                // syntex of accessing the refs
+                console.log(this.$refs.name);
+                this.$refs.name.classList.add("active");
+                this.$refs.name.focus();
+            },
+            toggleModal() {
+                this.showModal = !this.showModal;
+            },
+        },
+    };
 </script>
 
 <style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,29 +1,97 @@
 <template>
-    <div class="backdrop">
-        <div class="modal">
+    <div class="backdrop" @click.self="closeModal">
+        <!-- modal version 1.0 -->
+        <!-- <div class="modal" :class="{ sale: theme === 'sale' }">
+            <h1>{{ header }}</h1>
             <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Facere, perspiciatis.
+                {{ text }}
             </p>
+        </div> -->
+
+        <!-- modal version 2.0 -->
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <!-- this slots only works as default along with named slot
+            if we did not pass any content from app.vue 
+            though we did pass right now so this slot 👇 will not applied right now
+            but as long as we delete the content from app.vue this slot will apply -->
+
+            <slot>
+                <h3>default content</h3>
+            </slot>
+            <!-- we can also give name of slots. see App.vue for that -->
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
         </div>
     </div>
 </template>
 
+<script>
+    export default {
+        // for modal version 1.0
+        // props: ["header", "text", "theme"],
+
+        // for modal version .0
+        props: ["theme"],
+
+        methods: {
+            closeModal() {
+                this.$emit("close");
+            },
+        },
+    };
+</script>
+
 // style of that template element
 <style>
-.modal {
-    width: 400px;
-    padding: 20px;
-    margin: 100px auto;
-    background: #fff;
-    border-radius: 10px;
-}
+    .modal {
+        width: 686px;
+        padding: 3rem;
+        margin: 5rem auto;
+        background: #fff;
+        border-radius: 10px;
+    }
 
-.backdrop {
-    top: 0;
-    position: fixed;
-    background: rgba(0, 0, 0, 0.5);
-    width: 100%;
-    height: 100%;
-}
+    .modal p {
+        margin: 1rem 0 0 0;
+    }
+
+    .modal .actions {
+        text-align: center;
+        margin: 30px 0 10px 0;
+    }
+
+    .modal .actions a {
+        color: #333;
+        font-size: 1.2rem;
+        padding: 8px;
+        border: 1px solid #eee;
+        text-decoration: none;
+        margin: 10px;
+    }
+
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+
+    .modal.sale {
+        background: red;
+        color: #fff;
+    }
+
+    .modal .sale h1 {
+        color: #fff;
+    }
+
+    .modal.sale .actions {
+        color: #fff;
+    }
+
+    .modal.sale .actions a {
+        color: #fff;
+    }
 </style>
