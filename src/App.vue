8d61d4fbf984cb9f79784b3d746d36f53d398f65
mr-teslaa <mr.teslaa@protonmail.com>
2022-07-15 10:53:43 +0600

    project 1 reaction-timer | First part finished

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,50 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <h2>Salam from {{ boo }}</h2>
-    <hr />
-    <input type="text" ref="name" />
-    <button @click="handleClick">Change the input</button>
-
-    <!-- modal version 1.0 -->
-    <!-- <div v-if="showModal">
-        <Modal
-            :header="header"
-            :text="text"
-            theme="sale"
-            @close="toggleModal"
+    <h1>
+        Raction Timer with
+        <img
+            src="./assets/logo.png"
+            height="50"
+            width="50"
+            styles="margin-top: 1rem"
         />
-    </div> -->
+    </h1>
 
-    <!-- modal version 2.0 -->
-    <div v-if="showModal">
-        <Modal theme="sale" @close="toggleModal">
-            <!-- whatever template passing inside the MODAL tag is called slots -->
+    <button @click="start" :disabled="isPlaying">Play</button>
 
-            <!-- we can give name of slot -->
-            <template v-slot:links>
-                <a href="#">Take the offer</a>
-
-                <a href="#">Explore more giveaway</a>
-            </template>
-
-            <!-- if we delete this the default slot will work -->
-            <!-- <h1>Special Offer</h1>
-            <p>Grab your ninja swag for free 🎉</p> -->
-        </Modal>
-    </div>
-    <br />
-    <br />
-
-    <button @click.alt="toggleModal">Open Modal (alt+click)</button>
+    <!-- we can also use hardcoding v-bind:delay="delay" instead of :delay="dealy" -->
+    <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-    // importing the Modal.vue file
-    import Modal from "./components/Modal.vue";
+import Results from "./components/Results.vue";
+import Block from "./components/Block.vue";
 
-    export default {
-        name: "App",
-        components: { Modal },
-        data() {
-            return {
-                boo: "vue.js",
-                header: "Signup for the giveway",
-                text: "Grab your ninja swag fro half price",
-                showModal: false,
-            };
+export default {
+    name: "App",
+    components: { Block },
+    data() {
+        return {
+            isPlaying: false,
+            delay: null,
+        };
+    },
+    methods: {
+        start() {
+            this.delay = 2000 + Math.random() * 5000;
+            this.isPlaying = true;
+            console.log(this.delay);
         },
-
-        methods: {
-            handleClick() {
-                // syntex of accessing the refs
-                console.log(this.$refs.name);
-                this.$refs.name.classList.add("active");
-                this.$refs.name.focus();
-            },
-            toggleModal() {
-                this.showModal = !this.showModal;
-            },
-        },
-    };
+    },
+};
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="block">
+        <h3>click me again</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["delay"],
+};
+</script>
+
+<style>
+.block {
+    width: 30rem;
+    padding: 3rem;
+    margin: 1rem auto;
+    background: #0faf87;
+    color: #fff;
+    border-radius: 0.7rem;
+}
+</style>
